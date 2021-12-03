ebbb57bc5bd57febf46c5da5803e06aa9644af09
theanishjain <89607135+theanishjain@users.noreply.github.com>
2021-12-03 13:37:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+	<?php include('common/header.php') ?>
+	<div class="home-div"></div>
+		<div class="w-100 in-ad-ap">
+			<div class="row m-auto text-center">
+				<div class="col-md-4"><h3>VIT Vellore</h3></div>
+				<div class="col-md-4"><h3>VIT Chennai</h3></div>
+				<div class="col-md-4"><h3>VIT AP</h3></div>
+			</div>
+		</div>
+		<div class="paragraph">
+			<p>
+			VIT University was established with the aim of providing quality higher education on par with international standards. It persistently seeks and adopts innovative methods to improve the quality of higher education. The campus has a cosmopolitan atmosphere with students from all corners of the globe. Experienced and learned teachers are strongly encouraged to nurture the students.
+		</div>
+	<?php include('common/cards.php') ?>
+	<?php include('common/footer.php') ?>
+</body>
+</html>
+
