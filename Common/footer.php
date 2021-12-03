7c7a25ff9e1ddeb804e7c7424687acb419626454
theanishjain <89607135+theanishjain@users.noreply.github.com>
2021-12-03 13:42:14 +0530

    Add files via upload

---
+++ Common/footer.php
@@ -0,0 +1,42 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>VIT University</h3>
+            <p>VIT University was established under Section 3 of the University Grants Commission (UGC) Act, 1956. VIT Chennai was established in 2010. The University is headed by its founder and Chancellor, Dr. G. Viswanathan.
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About VIT</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.Tech. Civil Engineering</a></li>
+              <li><a href="">B.Tech. Electrical Engineering</a></li>
+              <li><a href="">B.Tech. Mechanical Engineering</a></li>
+              <li><a href="">B.Tech. Architecture</a></li>
+              <li><a href="">B.Tech. Computer Science</a></li>
+              <li><a href="">Bachelors of Business Administration</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: Vellore Institute of Technology,
+              Vandalur – Kelambakkam Road Chennai, Tamil Nadu – 600 127</p>
+              <p>Telephones: 91 44 3993 1555</p>
+              <p>E-mail: admin.chennai@vit.ac.in</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © reserved by Anish Jain 2021</p>
+</footer>
